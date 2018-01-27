a844593b6523d7fd5670912cae87f15549b00cc1
Josh Glover <jmglov@gmail.com>
2018-01-27 12:12:53 +0100

    Implement fractions/subtract

--- src/math/fractions.clj
+++ src/math/fractions.clj
@@ -1,4 +1,5 @@
 (ns math.fractions
+  (:require [math.core :as core])
   (:refer-clojure :exclude [numerator denominator]))
 
 (defn create [n d]
@@ -10,6 +11,24 @@
 (defn denominator [f]
   (second f))
 
+(defn multiply [a b]
+  (create (* (numerator a) (numerator b))
+          (* (denominator a) (denominator b))))
+
+(defn normalize [a b]
+  (let [a2 (multiply a (create (denominator b)
+                         (denominator b)))
+        b2 (multiply b (create (denominator a)
+                         (denominator a)))]
+    [a2 b2]))
+
 (defn add [a b]
-  (create (+ (numerator a) (numerator b))
-          (denominator a)))
+  (let [[a2 b2] (normalize a b)] 
+    (create (+ (numerator a2) (numerator b2))
+      (denominator a2))))
+
+(defn subtract [a b]
+  (let [[a2 b2] (normalize a b)] 
+    (create (- (numerator a2) (numerator b2))
+      (denominator a2))))
+
